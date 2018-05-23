3b7803e093c82ddfbc90544148fa232e7728d2cc
Darkmift <Darkmift@users.noreply.github.com>
2018-05-23 21:12:44 +0300

    added proper this declaration for sub scope usage

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
  fetch('animals.json')
-.then(response => response.json())
-.then(animals => initZoo(animals))
+     .then(response => response.json())
+     .then(animals => initZoo(animals))
 
-function initZoo(animalsData) {
+ function initZoo(animalsData) {
 
-	var foodFromJson = animalsData.animals.map(singleAnimalData => singleAnimalData.food);
-	var foodRangeArray = [...new Set(foodFromJson)];
-	var zoo = new Zoo(foodRangeArray);
+     var foodFromJson = animalsData.animals.map(singleAnimalData => singleAnimalData.food);
+     var foodRangeArray = [...new Set(foodFromJson)];
+     var zoo = new Zoo(foodRangeArray);
 
 
-	
-	//console.log(foodRange);
 
-	//zoo.setFoodRange(foodRange);
+     // console.log(foodRange);
 
-    animalsData.animals.forEach(singleAnimalData => {
-	 	var animal = new Animal(singleAnimalData);
-	 	console.log(animal);
-	 	zoo.buildAnimal(animal).appendTo(".row");//add animal to the zoo
-	})
-}
+     // zoo.setFoodRange(foodRange);
 
+     animalsData.animals.forEach(singleAnimalData => {
+         var animal = new Animal(singleAnimalData);
+         //console.log(animal);
+         zoo.buildAnimal(animal).appendTo(".row"); //add animal to the zoo
+     })
+ }
